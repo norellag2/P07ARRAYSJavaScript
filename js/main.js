c32e804884e1d911fc54320a4027e9e43a187679
norellag2 <norellag2@gmail.com>
2022-06-10 11:52:35 +0200

    ARRAYSJavaScript

---
+++ js/main.js
@@ -0,0 +1,15 @@
+//Arrays en JavaScript
+
+let frutas = ['pera', 'manzana', 'naranja'];
+console.log (frutas);
+
+//para acceder a los valores de un Array utilizamos su índice (que comienza en 0)
+
+//Para leer valores (posición o expresión)
+console.log  (frutas [0]);
+
+console.log (frutas [frutas.length -1]);
+
+//para escribir valores
+frutas [1] = 'albaricoque';
+console.log (frutas);
